c89c4dbac5a29a2718b48d095388be5e620240a1
John Smith <assaf@youappi.com>
2013-11-18 08:48:03 +0200

    before tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,16 @@
-module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  var gruntConfig = require('./grunt-config.json');
-  grunt.initConfig(gruntConfig);
-  grunt.registerTask('default', Object.keys(gruntConfig));
+var teamGenerator = require('./lib/generators/teamMembers');
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+    var gruntConfig = require('./grunt-config.json');
+    grunt.initConfig(gruntConfig);
+    grunt.registerTask('default', Object.keys(gruntConfig));
+
+    grunt.registerTask('generate-team-members', 'Generate team members from data file', function(){
+        teamGenerator(grunt);
+    });
 };
