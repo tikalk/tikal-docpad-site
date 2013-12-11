7e948a8455dd5892382fefe971b0ab147ea46718
gail <gail@gailsteiger.com>
2013-12-11 18:36:23 +0200

    tags

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-//    grunt.loadNpmTasks('grunt-contrib-clean');
 
 
     var gruntConfig = require('./grunt-config.json');
